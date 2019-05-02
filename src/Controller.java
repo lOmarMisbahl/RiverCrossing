d32160b8a2131cbb75f1c64dd9d5556e2273e0b1
Omar Misbah <xomarmisbahx@gmail.com>
2019-05-02 14:51:18 +0200

    Files

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,5 @@
 import Crossers.ICrosser;
-import Strategy.LevelOne;
 import Strategy.LevelThree;
-import Strategy.LevelTwo;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -161,7 +159,6 @@ public class Controller implements Initializable {
     void moveToBoat(ImageView A,Boolean fromLeftToRightBank){
 
         if (fromLeftToRightBank){
-            gameEngine.Command(save);
             LeftSide.getChildren().remove(A);
         }else {
             RightSide.getChildren().remove(A);
@@ -184,6 +181,7 @@ public class Controller implements Initializable {
 
             }
         }
+        gameEngine.Command(save);
     }
     @FXML
     void loadLevelOne(MouseEvent event) {
