6970e721a2bee88d364dd08541886a8fa6453fbb
NadineTarek <nadiinetarek98@yahoo.com>
2019-05-04 04:30:52 +0200

    level three done

--- src/Strategy/LevelThree.java
+++ src/Strategy/LevelThree.java
@@ -16,12 +16,14 @@ public class LevelThree implements ICrossingStrategy {
         boolean boatCanSail = false;
         for(int i=0;i<boatRiders.size();i++) {
             ICrosser x = boatRiders.get(i);
-            if ( x instanceof Farmer)
-            {
-                boatCanSail = true;
-            }
+            ICrosser y = rightBankCrossers.get(i);
+            if(Math.abs(x.getEatingRank()-y.getEatingRank())==1)
+                boatCanSail=false;
+            else
+                boatCanSail= true;
         }
 
+/*
         boolean rightBankError=false;
         boolean leftBankError=false;
         for (int i=0;(i<rightBankCrossers.size()-1);i++) {
@@ -44,9 +46,9 @@ public class LevelThree implements ICrossingStrategy {
                 else
                     leftBankError= false;
             }
-        }
+        }*/
 
-        if(boatCanSail && !leftBankError &&  !rightBankError)
+        if(boatCanSail )
             return true;
         else  return false;
 
@@ -55,20 +57,28 @@ public class LevelThree implements ICrossingStrategy {
     @Override
     public List<ICrosser> getInitialCrossers() {
         List<ICrosser> levelSceneRightBank = new ArrayList<ICrosser>();
-        Carnivorous wolf =new Wolf();
-        levelSceneRightBank.add(wolf);
-        Herbivorous sheep = new Sheep();
-        levelSceneRightBank.add(sheep);
-        Farmer farmer = new Farmer();
-        levelSceneRightBank.add(farmer);
-        Plant plant = new Plant();
-        levelSceneRightBank.add(plant);
+        Factory x = new Factory();
+        Crosser farmer1 = x.getCrosser("Farmer");
+        farmer1.setEatingRank(1);
+        levelSceneRightBank.add(farmer1);
+        Crosser farmer2 = x.getCrosser("Farmer");
+        farmer2.setEatingRank(2);
+        levelSceneRightBank.add(farmer1);
+        Crosser farmer3 = x.getCrosser("Farmer");
+        farmer3.setEatingRank(3);
+        levelSceneRightBank.add(farmer1);
+        Crosser farmer4 = x.getCrosser("Farmer");
+        farmer4.setEatingRank(4);
+        levelSceneRightBank.add(farmer1);
+        Crosser farmer5 = x.getCrosser("Farmer");
+        farmer5.setEatingRank(5);
+        levelSceneRightBank.add(farmer1);
         return levelSceneRightBank;
     }
 
     @Override
     public String[] getInstructions() {
-        String[] instructionOne = new String[]{"A farmer want to cross the river with a plant, a sheep and a wolf. The wolf and the sheep cant stay together on the right bank , the plant and the sheep cant stay together on the left bank. how to solve it? "};
-        return instructionOne;
+        String[] instructionThree = new String[]{" Five farmers want to cross the river but each one dislike his neighbor, the can only hold two people, how can they cross the river? "};
+        return instructionThree;
     }
 }
